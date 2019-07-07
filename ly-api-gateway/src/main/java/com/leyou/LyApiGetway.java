3de83adf3ca1f58d1836dacb90ff6988b605ddc2
Yuki <32598321+Yuki0011@users.noreply.github.com>
2019-07-07 16:17:59 +0800

    Update LyApiGetway.java

--- ly-api-gateway/src/main/java/com/leyou/LyApiGetway.java
+++ ly-api-gateway/src/main/java/com/leyou/LyApiGetway.java
@@ -1,1 +1,18 @@
-package com.leyou;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.cloud.client.discovery.EnableDiscoveryClient;import org.springframework.cloud.netflix.zuul.EnableZuulProxy;@SpringBootApplication@EnableDiscoveryClient@EnableZuulProxypublic class LyApiGetway {    public static void main(String[] args) {        SpringApplication.run(LyApiGetway.class,args);    }}
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class LyApiGetway {
+    public static void main(String[] args) {
+      //小袁来了
+      
+      
+        SpringApplication.run(LyApiGetway.class,args);
+    }
+}
