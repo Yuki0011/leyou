731210b48fb61b5a25cfa8e1f24b804be0be5b3e
xutao <18854218403@163.com>
2019-07-07 22:25:31 +0800

    测试冲突

--- ly-api-gateway/src/main/java/com/leyou/LyApiGetway.java
+++ ly-api-gateway/src/main/java/com/leyou/LyApiGetway.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class LyApiGetway {
     public static void main(String[] args) {
-      
+        //制造冲突 本地
         SpringApplication.run(LyApiGetway.class,args);
     }
 }
