7ff793ecd38cbc5e374bf673dc3230c6bd47517d
xutao <18854218403@163.com>
2019-07-07 22:36:26 +0800

    创建分支

--- ly-api-gateway/src/main/java/com/leyou/LyApiGetway.java
+++ ly-api-gateway/src/main/java/com/leyou/LyApiGetway.java
@@ -12,6 +12,7 @@ public class LyApiGetway {
     public static void main(String[] args) {
         //制造冲突  云端
         //制造冲突 本地
+        //创建分支
         SpringApplication.run(LyApiGetway.class,args);
     }
 }
